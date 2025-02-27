540caa55283e424ae7501e96680d3ae70a5654e2
László van den Hoek <laszlo.vandenhoek@gmail.com>
2025-02-27 11:25:47 +0100

    add aliases to put in .zshrc

--- zsh-llm-suggestions.zsh
+++ zsh-llm-suggestions.zsh
@@ -86,6 +86,10 @@ zsh_llm_suggestions_github_copilot() {
   zsh_llm_completion "$SCRIPT_DIR/zsh-llm-suggestions-github-copilot.py" "generate"
 }
 
+zsh_llm_suggestions_llm() {
+  zsh_llm_completion "$SCRIPT_DIR/zsh-llm-suggestions-llm.sh" "generate"
+}
+
 zsh_llm_suggestions_openai_explain() {
   zsh_llm_completion "$SCRIPT_DIR/zsh-llm-suggestions-openai.py" "explain"
 }
@@ -94,7 +98,14 @@ zsh_llm_suggestions_github_copilot_explain() {
   zsh_llm_completion "$SCRIPT_DIR/zsh-llm-suggestions-github-copilot.py" "explain"
 }
 
+zsh_llm_suggestions_llm_explain() {
+  zsh_llm_completion "$SCRIPT_DIR/zsh-llm-suggestions-llm.sh" "explain"
+}
+
 zle -N zsh_llm_suggestions_openai
 zle -N zsh_llm_suggestions_openai_explain
 zle -N zsh_llm_suggestions_github_copilot
 zle -N zsh_llm_suggestions_github_copilot_explain
+zle -N zsh_llm_suggestions_llm
+zle -N zsh_llm_suggestions_llm_explain
+
